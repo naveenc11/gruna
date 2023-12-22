e3ef268a8319bec234d4f4b54f54bfe839aef55e
n1chaudhary <n1chaudhary@bravurasolutions.com>
2023-12-22 13:50:28 +0530

    Initial Commit

---
+++ src/main/java/com/athens/gruna/GrunaApplication.java
@@ -0,0 +1,13 @@
+package com.athens.gruna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athens/gruna/GrunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athens.gruna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
